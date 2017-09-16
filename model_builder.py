bf2950b4bd0e170d8ad4196615ba16e9d4608cbf
Philippe Trempe <philippe-2.trempe@polymtl.ca>
2017-09-15 20:46:44 -0400

    Added a model builder for the Choosing a Model step.

---
+++ model_builder.py
@@ -0,0 +1,39 @@
+from keras import backend as b
+from keras.layers import Dense
+from keras.models import Sequential
+
+from hyperparameters import Hyperparameters
+
+
+class ModelBuilder(object):
+    @staticmethod
+    def build_model(input_shape, output_shape):
+        model = Sequential()
+
+        model.add(Dense(8, name="fcl1", input_shape=input_shape))
+        model.add(Dense(*output_shape, name="out"))
+
+        model.compile(
+            loss="mse",
+            optimizer="adam",
+            metrics=["mae", "acc", ModelBuilder.accuracy]
+        )
+
+        return model
+
+    @staticmethod
+    def get_model_custom_objects():
+        return {
+            "acc1": ModelBuilder.accuracy
+        }
+
+    @staticmethod
+    def accuracy(y_tilde, y_hat):
+        return b.mean(b.abs(b.round(y_hat) - y_tilde) <= 0.01)
+
+
+if __name__ == "__main__":
+    model = ModelBuilder.build_model(
+        Hyperparameters.INPUT_SHAPE, Hyperparameters.OUTPUT_SHAPE)
+    model.summary()
+    print(model.get_weights())
